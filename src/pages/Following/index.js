7aea0a4c6d787740466ea468415d7e82a6ae8b81
Chuong nguyen <nguyenchuong.1203@gmail.com>
2022-07-23 23:30:41 +0700

    Layout

---
+++ src/pages/Following/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Following(props) {
+    return <h2>Following page</h2>;
+}
+
+export default Following;
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Profile(props) {
+    return <h2>Profile page</h2>;
+}
+
+export default Profile;
